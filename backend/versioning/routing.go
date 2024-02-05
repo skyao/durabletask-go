0517ef75c38856637bff1171dc21d5e617d0c94b
Sky Ao <aoxiaojian@gmail.comx>
2024-02-05 09:38:43 -0500

    add GetWorkflowRevision() method to get revision
    Signed-off-by: Sky Ao <aoxiaojian@gmail.comx>

--- backend/versioning/routing.go
+++ backend/versioning/routing.go
@@ -2,6 +2,7 @@ package versioning
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 )
 
@@ -11,6 +12,10 @@ func SaveReportedWorkflowRevisions(revisions map[string]int32) {
 	for name, revision := range revisions {
 		routing.add(name, int(revision))
 	}
+}
+
+func GetWorkflowRevision(workflowName string) int {
+	return routing.get(workflowName)
 }
 
 type revisionRouting struct {
@@ -28,8 +33,29 @@ func (r *revisionRouting) add(name string, revision int) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 
-	fmt.Printf("**** versioning ****: saved workflow revision for routing: name=%s, revision=%d\n", 
+	fmt.Printf("**** versioning ****: saved workflow revision for routing: name=%s, revision=%d\n",
 		name, revision)
 
 	r.revisions[name] = revision
+}
+
+func (r *revisionRouting) get(name string) int {
+	r.lock.RLock()
+	defer r.lock.RUnlock()
+
+  // name is actor type, value like "dapr.internal.default.WorkflowConsoleApp.workflow"
+  // key of r.revisions is workflow name, like "io.dapr.quickstarts.workflow.OrderProcessingWorkflow"	
+  key := name	
+	if strings.Contains(key, "WorkflowConsoleApp") && strings.HasSuffix(key, ".workflow") {
+		key = "io.dapr.quickstarts.workflows.OrderProcessingWorkflow"
+	} else if strings.Contains(key, "TestConsoleApp") {
+		key = "io.dapr.quickstarts.workflows.TestWorkflow"
+	}
+ 
+  revision, ok := r.revisions[key]
+	if !(ok) {
+		revision = DEFAULT_REVISION
+	}
+
+	return revision
 }
